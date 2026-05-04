b8fcf805d0b929051bcbaaebe8377f550d860648
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-05-04 18:32:39 -0300

    add constructor

--- src/main/java/Escolaridade.java
+++ src/main/java/Escolaridade.java
@@ -1,6 +1,10 @@
 public class Escolaridade {
     private String nome
 
+    public Escolaridade(String nome) {
+        this.nome = nome;
+    }
+
     public String getNome() {
         return nome;
     }
