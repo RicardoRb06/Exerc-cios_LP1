0b512a8677aa5b5f165bc1d4ef9a5d70bbeb8b36
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-05-04 19:09:15 -0300

    change test qualEscoladidadeProfessor

--- src/test/java/ProfessorTest.java
+++ src/test/java/ProfessorTest.java
@@ -6,9 +6,8 @@ class ProfessorTest {
 
     @Test
     public void qualEscolaridadeProfessor(){
-        Professor professor = new Professor();
-        professor.setEscolaridade(new Escolaridade("doutor"));
-        assertEquals("doutor", professor.obterEscolaridade());
+        Professor professor = new Professor(new Escolaridade("doutorado"));
+        assertEquals("doutorado", professor.obterEscolaridade());
     }
 
 }
