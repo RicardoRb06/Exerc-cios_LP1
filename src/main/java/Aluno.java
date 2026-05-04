c45abfbbb09c392543b1f60f1e4d3e8a0e666bef
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-05-04 20:47:12 -0300

    create class

---
+++ src/main/java/Aluno.java
@@ -0,0 +1,9 @@
+public class Aluno extends Pessoa{
+    public Aluno(Cidade naturalidade) {
+        super(naturalidade);
+    }
+
+    public Aluno(Cidade naturalidade, String escolaridade) {
+        super(naturalidade, escolaridade);
+    }
+}
