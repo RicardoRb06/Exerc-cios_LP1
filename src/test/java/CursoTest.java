027a8437472f05fd8c1c95c0f45705e9634b55d3
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-05-04 19:25:49 -0300

    crate test class

---
+++ src/test/java/CursoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CursoTest {
+
+    @Test
+    public void qualEscolaridadeCoordenador() {
+        Curso curso = new Curso(new Professor(new Escolaridade("doutorado")));
+        assertEquals("doutorado", curso.obterEscolaridadeCoordenador());
+    }
+
+    @Test
+    public void verificaErroCoordenadorNull() {
+        try{
+            Curso curso = new Curso(null);
+            fail();
+        } catch (NullPointerException e){
+            assertEquals("Coordenador não pode ser null", e.getMessage());
+        }
+    }
+
+}
