8bd294d909987c5ed10d921bfeca1c3522ebde1c
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-05-04 21:14:01 -0300

    add curso atribute to aluno

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -1,9 +1,26 @@
 public class Aluno extends Pessoa{
+    private Curso curso;
+
     public Aluno(Cidade naturalidade) {
         super(naturalidade);
     }
 
     public Aluno(Cidade naturalidade, String escolaridade) {
         super(naturalidade, escolaridade);
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public String obterEstadoEstuda(){
+        if (this.curso == null) {
+            throw new NullPointerException("Curso não pode ser null")
+        }
+        this.curso.obterEstado();
     }
 }
