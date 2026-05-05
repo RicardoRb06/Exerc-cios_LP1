986fa032fbe8f7ceaa602d4ad9246d20e3d3c5f1
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-05-04 21:23:36 -0300

    add qualEstadoEstuda

--- src/test/java/AlunoTest.java
+++ src/test/java/AlunoTest.java
@@ -9,4 +9,14 @@ class AlunoTest {
         Aluno aluno = new Aluno(cidade);
         assertEquals("MG", aluno.obterEstadoNaturalidade());
     }
+
+    @Test
+    public void qualEstadoEstuda() {
+        Cidade cidade = new Cidade("Santos Dumont", "MG");
+        Cidade cidade1 = new Cidade("Rio de janeiro", "RJ");
+        Curso curso = new Curso(cidade, new Professor(cidade, "doutorado"));
+        Aluno aluno = new Aluno(cidade);
+        aluno.setCurso(curso);
+        assertEquals("RJ", aluno.obterEstadoEstuda());
+    }
 }
